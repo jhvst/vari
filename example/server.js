0ce409a402fecb56ca94b3eeef5de08820cf67e5
Juuso Haavisto <juuso@mail.com>
2014-05-06 20:24:01 +0300

    fix example

--- example/server.js
+++ example/server.js
@@ -4,9 +4,7 @@ var vari = require("../main.js")
 
 //I currently have no idea why this does not work.
 http.createServer(function(request, response) {  
-	var cvar = new Vari()
-	var captcha = cvar.Template()
     response.writeHeader(200, {"Content-Type": "text/html"});  
-    response.write(captcha);  
+    response.write(vari);
     response.end();
 }).listen(8000);
